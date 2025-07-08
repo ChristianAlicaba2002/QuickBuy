3e72fce15851c449abc6c0d00394715ad4c174cc
Christian <christiandave120702@gmail.com>
2025-07-08 16:27:20 +0800

    Implement the Eloquent Providor in Order

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use App\Domain\Product\ProductRepository;
 use App\Infrastructure\Eloquent\Product\EloquentProductRepository;
 use App\Domain\AddToCart\AddToCartRepository;
 use App\Infrastructure\Eloquent\AddToCart\EloquentAddToCartRepository;
+use App\Domain\Order\OrderRepository;
+use App\Infrastructure\Eloquent\Order\EloquentOrderRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,8 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(ProductRepository::class , EloquentProductRepository::class);
-        $this->app->bind(AddToCartRepository::class , EloquentAddToCartRepository::class);
+        $this->app->bind(ProductRepository::class, EloquentProductRepository::class);
+        $this->app->bind(AddToCartRepository::class, EloquentAddToCartRepository::class);
+        $this->app->bind(OrderRepository::class, EloquentOrderRepository::class);
     }
 
     /**
